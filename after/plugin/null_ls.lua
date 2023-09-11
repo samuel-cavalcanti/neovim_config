fc3dd129f65d260ae99dc3bb1aab74b3a5a90c80
samuel-cavalcanti <scavalcanti111@gmail.com>
2023-09-11 10:21:28 -0300

    disabilitado eslint e habilitado pritter

--- after/plugin/null_ls.lua
+++ after/plugin/null_ls.lua
@@ -3,7 +3,8 @@ local null_ls = require("null-ls")
 null_ls.setup({
     sources = {
         null_ls.builtins.formatting.autopep8,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.formatting.eslint,
+        null_ls.builtins.formatting.prettier
+        --null_ls.builtins.diagnostics.eslint,
+        --null_ls.builtins.formatting.eslint,
     },
 })
