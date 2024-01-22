4f07d7af5b92e9f391160101f77cfe9c4d2ed033
samuel-cavalcanti <scavalcanti111@gmail.com>
2024-01-22 12:14:38 -0300

    updated tree dependences in autocomplete

--- lua/lazy_nvim/plugins.lua
+++ lua/lazy_nvim/plugins.lua
@@ -1,6 +1,6 @@
 return {
-    { 'nvim-tree/nvim-web-devicons' },
-    { 'romgrk/barbar.nvim',         dependencies = 'nvim-web-devicons' },
+    { 'kyazdani42/nvim-web-devicons' },
+    { 'romgrk/barbar.nvim',          dependencies = 'nvim-web-devicons' },
 
     {
         'nvim-telescope/telescope.nvim',
@@ -36,18 +36,26 @@ return {
             { 'neovim/nvim-lspconfig' },             -- Required
             { 'williamboman/mason.nvim' },           -- Optional
             { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+            {
+                'hrsh7th/nvim-cmp',                  -- Autocompletion Required
+                dependencies = {
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },         -- Required
-            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
-            { 'hrsh7th/cmp-buffer' },       -- Optional
-            { 'hrsh7th/cmp-path' },         -- Optional
-            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-            { 'hrsh7th/cmp-nvim-lua' },     -- Optional
+                    { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+                    { 'hrsh7th/cmp-buffer' },       -- Optional
+                    { 'hrsh7th/cmp-path' },         -- Optional
+                    { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+                    { 'hrsh7th/cmp-nvim-lua' },     -- Optional
 
-            -- Snippets
-            { 'L3MON4D3/LuaSnip' },             -- Required
-            { 'rafamadriz/friendly-snippets' }, -- Optional
+                    -- Snippets
+                    {
+                        'L3MON4D3/LuaSnip',                     -- Required
+                        dependencies = {
+                            { 'rafamadriz/friendly-snippets' }, -- Optional
+                        }
+                    },
+                }
+            },
+
         }
     },
 
