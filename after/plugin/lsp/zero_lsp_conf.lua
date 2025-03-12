a90c6bd26bee1a75e5c6ff4e205c0af741321fee
samuel-cavalcanti <scavalcanti111@gmail.com>
2025-03-12 19:24:37 -0300

    feat: disable hits for java

--- after/plugin/lsp/zero_lsp_conf.lua
+++ after/plugin/lsp/zero_lsp_conf.lua
@@ -1,12 +1,19 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-    lsp_zero.default_keymaps({ buffer = bufnr })
-    vim.lsp.inlay_hint.enable(true)
+        lsp_zero.default_keymaps({ buffer = bufnr })
 
-    vim.keymap.set({ 'n', 'x' }, '<F3>', function()
-        vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
-    end)
+        if client.name == 'jdtls' or client.name == 'spring-boot' then
+                vim.lsp.inlay_hint.enable(false)
+        else
+                vim.lsp.inlay_hint.enable(true)
+        end
+
+
+
+        vim.keymap.set({ 'n', 'x' }, '<F3>', function()
+                vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+        end)
 end)
 
 local lsp_config = require('lspconfig')
@@ -16,4 +23,3 @@ lsp_config.lua_ls.setup(lsp_zero.nvim_lua_ls())
 
 
 lsp_zero.setup()
-
